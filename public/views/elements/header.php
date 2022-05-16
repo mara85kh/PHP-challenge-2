44f6f91684ecd34bc9205eeab6c7b5d989132701
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-16 10:19:37 +0200

    add header, style

---
+++ public/views/elements/header.php
@@ -0,0 +1,33 @@
+<!-- header-user -->
+
+<nav class="navbar navbar-expand-lg ">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="../../assets/img/logo.svg" alt="Logo" width="30" height="24">
+      <span class="menu_logo_text">COGIP</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav mb-2 ">
+        <li class="nav-item ">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Factures</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sociétés</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Connexion</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
